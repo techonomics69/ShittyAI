18e21f97cfa75c04e5d7d012f8cd8050ff8d47ad
Christian Anderson <christian@techonomics.org>
2023-10-19 17:38:06 -0700

    Update brand.ts

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -8,22 +8,22 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
+    Base: 'Shitty AI',
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
     Description: 'Leading open-source AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Harnessing AI for You',
+    SiteName: 'Shitty AI | Harnessing AI for You',
     ThemeColor: '#32383E',
-    TwitterSite: '@enricoros',
+    TwitterSite: '@shittyai',
   },
   URIs: {
-    Home: 'https://big-agi.com',
+    Home: 'https://shittyai.com',
     // App: 'https://get.big-agi.com',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    SupportInvite: 'https://discord.gg/zKpRdny5',
     // Twitter: 'https://www.twitter.com/enricoros',
-    PrivacyPolicy: 'https://big-agi.com/privacy',
+    PrivacyPolicy: 'https://shittyai.com/privacy',
   },
 };
